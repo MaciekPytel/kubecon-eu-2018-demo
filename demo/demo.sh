6a19e7caf41fbc298d09800a365cea096aa43d7b
MaciekPytel <maciekpytel@google.com>
2018-05-03 15:52:39 +0200

    Last minute demo update

--- demo/demo.sh
+++ demo/demo.sh
@@ -7,12 +7,16 @@ DEMO[1]="kubectl create -f hpa.yaml"
 DEMO[2]="cat patch1.yaml"
 DEMO[3]='kubectl patch hpa.v2beta1.autoscaling sample-server --patch "$(cat patch1.yaml)"'
 DEMO[4]="kubectl create -f ../adapter-config/custom-metrics-apiserver-deployment.yaml"
+DEMO[5]="cat patch2.yaml"
+DEMO[6]='kubectl patch hpa.v2beta1.autoscaling sample-server --patch "$(cat patch2.yaml)"'
 
 BREAK[0]=false
 BREAK[1]=true
 BREAK[2]=false
 BREAK[3]=true
 BREAK[4]=true
+BREAK[5]=false
+BREAK[6]=true
 
 for i in $(seq ${STEP} ${#DEMO[@]})
 do
